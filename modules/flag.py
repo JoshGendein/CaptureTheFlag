6be171f211a16a388fc123b19fe0e3554f35c1fe
Josh Gendein <joshua@gendein.com>
2020-03-04 08:36:57 -0800

    now tracks its position + added observation step

--- modules/flag.py
+++ modules/flag.py
@@ -7,20 +7,22 @@ class Flag(EnvModule):
 
     def __init__(self, flag_size=2):
         self.flag_size = flag_size
+        self.pos = []
 
     def build_world_step(self, world):
-        self.pos = placement_fn(world.grid_size, world.placement_grid, obj_size=(self.flag_size, self.flag_size))
+        self.initial_pos = placement_fn(world.grid_size, world.placement_grid, obj_size=(self.flag_size, self.flag_size))
 
         for i in range(self.flag_size):
             for j in range(self.flag_size):
-                world.placement_grid[self.pos[0] + i][self.pos[1]+ j] = 1
+                world.placement_grid[self.initial_pos[0] + i][self.initial_pos[1]+ j] = 1
+                self.pos.append((self.initial_pos[0] + i, self.initial_pos[1]+ j))
         
         return True
 
     def build_render(self, viewer, block_size):
-        l = self.pos[0] * block_size
+        l = self.initial_pos[0] * block_size
         r = l + (block_size * self.flag_size)
-        b = self.pos[1] * block_size
+        b = self.initial_pos[1] * block_size
         t = b + (block_size * self.flag_size)
 
         current_block = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
@@ -29,4 +31,12 @@ class Flag(EnvModule):
 
         return True
 
+    def observation_step(self, world):
+        obs = {
+            'flag_pos': []
+        }
+
+        obs['flag_pos'] = self.pos
+
+        return obs
 
