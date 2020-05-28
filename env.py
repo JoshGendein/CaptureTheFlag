e889d98139e6a9b616b4864950d7b0cf7be64b3d
Josh Gendein <joshua@gendein.com>
2020-05-28 09:21:43 -0700

    Restructured directories.

--- env.py
+++ env.py
@@ -1,7 +1,5 @@
 import gym
 import numpy as np
-from world import World
-from gym import spaces
 from gym.envs.classic_control import rendering
 
 class Env(gym.Env):
@@ -9,56 +7,32 @@ class Env(gym.Env):
         'render.modes': ['human'],
     }
 
-    def __init__(self, world=World(), horizon=250, screen_size=512):
-        self.vec_actions = {
-            0 : (0, 0),
-            1: (0, 1),
-            2: (0, -1),
-            3: (-1, 0),
-            4: (1, 0)
-        }
-        self.horizon = horizon
+    def __init__(self, get_world, get_render, horizon=250, screen_size=512):
+        self.get_world = get_world
+        self.get_render = get_render
+
         self.screen_size = screen_size
 
-        self.world = world
+        self.world = None
         self.viewer = None
-        self.action_space = spaces.Discrete(5)
 
     def step(self, action):
-        actions = []
-        for agent_action in action:
-            assert self.action_space.contains(agent_action)
-
-            vec_action = self.vec_actions[agent_action]
-            actions.append(vec_action)
-
-        self.world.set_action(actions)
-        obs = self.world.get_observation()
-        reward = 0
-        done = False
-        
-
-        for pos in obs['agent_pos']:
-            if(pos in obs['flag_pos']):
-                done = True
-
-        return obs, reward, done, None
+        pass
 
     def render(self, mode='human'):
         if mode == 'human':
             if self.viewer is None:
                 self.viewer = rendering.Viewer(self.screen_size, self.screen_size)
             
-                self.world.get_render(self.viewer)
+            self.get_render(self.viewer)
             self.viewer.render()
         else:
             raise ValueError("Unsupported mode.")
 
     def reset(self):
-        self.world.reset()
-    
+        self.world = self.get_world()
+
     def close(self):
         if self.viewer:
             self.viewer.close()
             self.viewer = None
-
--- example.py
+++ example.py
@@ -1,18 +1,17 @@
 import numpy as np
 import time
-from modules.walls import Walls
-from modules.agents import Agents
-from modules.flag import Flag
-from world import World
-from env import Env
+from environment.world import World
+from environment.env import Env
 
+np.set_printoptions(threshold=np.inf)
 
 grid_size = 32
 
-world = World(grid_size=grid_size, n_agents=1)
 
-env = Env(world=world)
-env.reset()
+env = Env(grid_size=grid_size, n_agents=1, flag_size=1)
+initial = env.reset()
+
+start = time.time()
 
 for _ in range(env.horizon): 
     env.render()
@@ -21,4 +20,10 @@ for _ in range(env.horizon):
         print("Got to the flag!")
         env.close()
         break
-    time.sleep(1 / env.horizon)
+    # time.sleep(1 / env.horizon)
+
+env.close()
+
+end = time.time()
+
+print(end-start)
